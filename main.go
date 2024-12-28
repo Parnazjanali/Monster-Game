005c3ade02a8e1208dc1e9ce20c4c3e519c9effa
parnaz janali <Janaliparnaz@gmail.com>
2024-12-28 16:07:16 +0330

    Second update with simple function

--- main.go
+++ main.go
@@ -1,43 +1,49 @@
 package main
 
 import (
+	"fmt"
+
 	interactions "github.com/parnazjanali/Monstergame/Interactions"
 	"github.com/parnazjanali/Monstergame/actions"
 )
 
 func main() {
-	startGame()
-	playingGame()
-
-}
-func startGame() {
 	interactions.PrintInstructions()
 
-}
-func playingGame() {
-	userChoice := interactions.GetLayerchoice()
-	var playerAttackdmg int
-	var monsterAttackdmg int
-	var healPlayer int
+	result := interactions.GetLayerchoice()
+	var playerAttackDmg, monsterAttackDmg, healAmount int
 
-	if userChoice == "Attack" {
-		playerAttackdmg = actions.Playerattack()
+	switch result {
+	case "Attack":
+		playerAttackDmg = actions.Playerattack()
+		monsterAttackDmg = actions.Monsterattack()
+
+		fmt.Printf("You attacked the monster and dealt %d damage.\n", playerAttackDmg)
+		fmt.Printf("The monster attacked you and dealt %d damage.\n", monsterAttackDmg)
+
+	case "Heal":
+		healAmount = actions.Playerheal()
+		monsterAttackDmg = actions.Monsterattack()
+
+		fmt.Printf("You healed yourself for %d health.\n", healAmount)
+		fmt.Printf("The monster attacked you and dealt %d damage.\n", monsterAttackDmg)
+
+	default:
+		fmt.Println("Your choice is invalid type!")
+		return
+	}
+	playerHealth, monsterHealth := actions.GethealthAmount()
+
+	fmt.Printf("Player's health: %d\n", playerHealth)
+	fmt.Printf("Monster's health: %d\n", monsterHealth)
+
+	if playerHealth <= 0 {
+		fmt.Println("You lost! The monster won.")
+
+	} else if monsterHealth <= 0 {
+		fmt.Println("Monster lost! You won the game.")
+
 	} else {
-		healPlayer = actions.Playerheal()
+		fmt.Println("continue!")
 	}
-	monsterAttackdmg = actions.Monsterattack()
-
-	playerhealth, monsterhealth := actions.GethealthAmount()
-
-	data := interactions.Output{
-		Action:        userChoice,
-		Playerhealth:  playerhealth,
-		Monsterhealth: monsterhealth,
-		AttackMonster: monsterAttackdmg,
-		AttackPlayer:  playerAttackdmg,
-		HealPlayer:    healPlayer,
-	}
-
-	interactions.OutputResult(&data)
-
 }
